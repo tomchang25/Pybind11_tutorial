6c37baeff09c72160c3c38f91673c4d9a10bd225
unknown <tomchang25@gmail.com>
2019-12-23 15:44:43 +0800

    cout redirct v5

--- 1_stl/Build.cpp
+++ 1_stl/Build.cpp
@@ -7,14 +7,9 @@
 namespace py = pybind11;
 
 PYBIND11_MODULE(STL, m) {   // module import name, module object
-    // Add a scoped redirect for your noisy code
-    m.def("noisy_func", []() {
-        py::scoped_ostream_redirect stream(
-            std::cout,                               // std::ostream&
-            py::module::import("sys").attr("stdout") // Python output
-        );
-        call_noisy_func();
-    });
+    // redirct stdout to py::stdout
+    m.attr("redirect_output") = py::capsule(new py::scoped_output_redirect(...),
+    [](void *sor) { delete static_cast<py::scoped_output_redirect *>(sor); });
 
     m.doc() = "STL tutorial";      // module doc string 
     m.def("randomint1D", &randomint1D);

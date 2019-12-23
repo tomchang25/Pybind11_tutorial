dee6da0d761a54d43d96877bcced897885a893be
unknown <tomchang25@gmail.com>
2019-12-23 15:06:47 +0800

    stl cpp_v3

--- 1_stl/Build.cpp
+++ 1_stl/Build.cpp
@@ -1,9 +1,24 @@
 // pybind11_exp.cpp
 #include "STL.h"
 
-PYBIND11_MODULE(STL, m) {   // module import name, module object
-  m.doc() = "STL tutorial";      // module doc string
+#include <pybind11/pybind11.h>
+#include <pybind11/iostream.h>
+namespace py = pybind11;
 
-  m.def("randomint1d", &randomint1d);
-  m.def("zeros2D", &zeros2D);
+PYBIND11_MODULE(STL, m) {   // module import name, module object
+    // Add a scoped redirect for your noisy code
+    m.def("noisy_func", []() {
+        py::scoped_ostream_redirect stream(
+            std::cout,                               // std::ostream&
+            py::module::import("sys").attr("stdout") // Python output
+        );
+        call_noisy_func();
+    });
+
+    m.doc() = "STL tutorial";      // module doc string 
+    m.def("randomint1d", &randomint1d);
+    m.def("zeros2D", &zeros2D);
 }
+
+...
+
--- 1_stl/STL.cpp
+++ 1_stl/STL.cpp
@@ -1,23 +1,7 @@
 #include "STL.h"
 
 void print(string str) {
-    #ifndef USE_PYTHON
-        cout << str << endl;
-    #endif
-
-    #ifdef USE_PYTHON
-        py::print(str);
-    #endif
-}
-
-void write(string str) {
-    #ifndef USE_PYTHON
-        cout << str;
-    #endif
-
-    #ifdef USE_PYTHON
-        py::print(str, end = '');
-    #endif
+    cout << str << endl;
 }
 
 vector<int> randomint1d(int low, int high, int size){
@@ -49,16 +33,16 @@ vector<vector<int>> zeros2D(tuple<int,int> shape){
 int main(){
     for ( const auto &row : randomint1d(-5,88,7) )
     {
-        write(to_string(row) + " ");
+        cout << row << " ";
     }
-    print("");
+    cout << endl;
 
     for ( const auto &row : zeros2D({3,4}) )
     {
         for ( const auto &s : row ) {
-            write(to_string(s) + " ");
+            cout << s << " ";
         }
-        print("");
+        cout << endl;
     }
 
     return 1;
